5a5d658c36492ab33fd52b8d054a2d5cb0aba843
alejorojas012 <alejo.rojas012@gmail.com>
2026-04-20 21:28:53 -0500

    agregar proxy vercel

---
+++ api/proxy.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  const path = req.url.replace('/api', '')
+  const url = `https://gb.starthing.com/gw/merchant${path}`
+  
+  const response = await fetch(url, {
+    method: req.method,
+    headers: {
+      'Ram-System': '1144269879315968000',
+      'Ram-Tenant': '1405022612241514496',
+      'Ram-Token': 'df9a6092108647649aed55ce0e51f55b1495927437289426944',
+      'X-Accept-Language': 'es',
+      'Content-Type': 'application/json',
+    },
+  })
+  
+  const data = await response.json()
+  res.status(200).json(data)
+}
