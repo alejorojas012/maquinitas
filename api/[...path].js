25dfb1036fe839b0a88f67e453384c4e2400897b
alejorojas012 <alejo.rojas012@gmail.com>
2026-04-20 21:35:01 -0500

    fix vercel proxy

--- api/[...path].js
+++ api/[...path].js
@@ -1,9 +1,10 @@
 export default async function handler(req, res) {
-  const path = req.url.replace('/api', '')
-  const url = `https://gb.starthing.com/gw/merchant${path}`
-  
+  const url = req.url
+  const apiPath = url.replace(/^\/api\/proxy/, '').replace(/^\/api/, '')
+  const target = `https://gb.starthing.com/gw/merchant${apiPath}`
+
   try {
-    const response = await fetch(url, {
+    const response = await fetch(target, {
       method: req.method,
       headers: {
         'Ram-System': '1144269879315968000',
@@ -14,6 +15,7 @@ export default async function handler(req, res) {
       },
     })
     const data = await response.json()
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.status(200).json(data)
   } catch(e) {
     res.status(500).json({ error: e.message })
